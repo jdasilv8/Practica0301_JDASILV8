a71c8b304ba43d7f99b397bddcf6f35dba24e459
jorgito <jdasilv8@educacion.navarra.es>
2024-01-11 16:44:30 +0100

    a

---
+++ Ejercicio1Bucle.py
@@ -0,0 +1,21 @@
+import datetime
+
+start_time = datetime.datetime.now()
+
+num = int(input("Indique un número entero: "))
+def FibonacciBucles (n):
+    if n <= 1:
+        return n
+    else:
+        a = 0
+        b = 1
+        for i in range (n-1):
+            c = a + b
+            a = b
+            b = c
+        return c
+
+fibonacci = FibonacciBucles(num)
+print ("El número de fibonacci de " + str(num) + " es " + str(fibonacci))
+end_time = datetime.datetime.now()
+print ("El tiempo de ejecución es:", end_time - start_time)
---
+++ Ejercicio1FunRecursiva.py
@@ -0,0 +1,15 @@
+import datetime
+
+start_time = datetime.datetime.now()
+
+num = int(input("Indique un número entero: "))
+def FibonacciRecur (n):
+    if n <= 1:
+        return n
+    else:
+        return FibonacciRecur(n-1) + FibonacciRecur(n-2)
+
+fibonacci = FibonacciRecur(num)
+print ("El número de fibonacci de " + str(num) + " es " + str(fibonacci))
+end_time = datetime.datetime.now()
+print ("El tiempo de ejecución es:", end_time - start_time)
