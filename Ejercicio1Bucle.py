7aab7d1b0d97404a0e9cb88e6de0c2575579a574
jorgito <jdasilv8@educacion.navarra.es>
2024-01-15 19:37:13 +0100

    a

--- Ejercicio1Bucle.py
+++ Ejercicio1Bucle.py
@@ -1,8 +1,12 @@
 import datetime
 
 start_time = datetime.datetime.now()
-
 num = int(input("Indique un número entero: "))
+fibonacci = FibonacciBucles(num)
+print ("El número de fibonacci de " + str(num) + " es " + str(fibonacci))
+end_time = datetime.datetime.now()
+print ("El tiempo de ejecución es:", end_time - start_time)
+
 def FibonacciBucles (n):
     if n <= 1:
         return n
@@ -14,8 +18,3 @@ def FibonacciBucles (n):
             a = b
             b = c
         return c
-
-fibonacci = FibonacciBucles(num)
-print ("El número de fibonacci de " + str(num) + " es " + str(fibonacci))
-end_time = datetime.datetime.now()
-print ("El tiempo de ejecución es:", end_time - start_time)
